6e8d85e4ca85e31aa37b3dcaf06cc152e834278a
Hinal05 <hinal.parikh@qed42.com>
2025-09-22 20:04:54 +0530

    cart-01: Rep initiated cart updated.

--- src/Cart/rep/index.jsx
+++ src/Cart/rep/index.jsx
@@ -1,10 +1,8 @@
-import RepHeader from "../../components/Header/repHeader";
 import { RepCartDropdown } from "../../components/organisms/repCartDropdown"
 
 export default function RepCartHome() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <RepHeader />
       <main className="p-4">
         <RepCartDropdown variant="fullpage" />
       </main>
